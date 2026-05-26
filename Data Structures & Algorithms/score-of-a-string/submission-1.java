3f9f8527fcf512910ffbbc4f4af4794264982434
Rutwik2609 <98237672+Rutwik2609@users.noreply.github.com>
2026-05-26 12:47:44 +0530

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i = 1; i < s.length(); i++) {
+            //System.out.println("sum += " + ((int)s.charAt(i) - (int)s.charAt(i - 1)));
+            sum += Math.abs(s.charAt(i) - s.charAt(i - 1)); 
+        }
+        return sum;
+    }
+}
